6b2da0e6da37cee2a9143083ace61990d0e2b83a
pakwan118 <thierry.laforge@ucalgary.ca>
2022-04-01 01:09:31 -0600

    Add communications with arduino

--- yeet/mediapipe_alt.py
+++ yeet/mediapipe_alt.py
@@ -1,56 +1,71 @@
 import cv2
 import mediapipe as mp
+import serial
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_pose = mp.solutions.pose
+arduino = serial.Serial("COM5", 115200, timeout=1)
 
-cam = cv2.VideoCapture(0)
-with mp_pose.Pose(min_detection_confidence=0.7, min_tracking_confidence=0.7) as pose:
-    while cam.isOpened():
-        ret, frame = cam.read()
-        frame.flags.writeable = True # Can fuck around with this
-        results = pose.process(frame)
 
-        if not results.pose_landmarks:
-            print("No model found")
+def send_coords(coords):
+    print(f"Sending {coords[0]} {coords[1]} to arduino...")
+    arduino.write(f"{coords[0]} {coords[1]}\n".encode())
 
-        else:
-            frame_h, frame_w, _ = frame.shape
-            right_hip_x = results.pose_landmarks.landmark[24].x * frame_w
-            right_hip_y = results.pose_landmarks.landmark[24].y * frame_h
 
-            left_hip_x = results.pose_landmarks.landmark[23].x * frame_w
-            left_hip_y = results.pose_landmarks.landmark[23].y * frame_h
+try:
+    cam = cv2.VideoCapture(0)
+    with mp_pose.Pose(min_detection_confidence=0.7, min_tracking_confidence=0.7) as pose:
+        while cam.isOpened():
+            ret, frame = cam.read()
+            frame.flags.writeable = False # Can fuck around with this
+            results = pose.process(frame)
 
-            right_knee_x = results.pose_landmarks.landmark[26].x * frame_w
-            right_knee_y = results.pose_landmarks.landmark[26].y * frame_h
+            if not results.pose_landmarks:
+                pass
 
-            left_knee_x = results.pose_landmarks.landmark[25].x * frame_w
-            left_knee_y = results.pose_landmarks.landmark[25].y * frame_h
+            else:
+                frame_h, frame_w, _ = frame.shape
+                right_hip_x = results.pose_landmarks.landmark[24].x * frame_w
+                right_hip_y = results.pose_landmarks.landmark[24].y * frame_h
 
-            cv2.circle(frame, [int(right_hip_x), int(right_hip_y)], 3, [0, 255, 0], -1)
-            cv2.circle(frame, [int(left_hip_x), int(left_hip_y)], 3, [0, 255, 0], -1)
+                left_hip_x = results.pose_landmarks.landmark[23].x * frame_w
+                left_hip_y = results.pose_landmarks.landmark[23].y * frame_h
 
-            cv2.circle(frame, [int(right_knee_x), int(right_knee_y)], 3, [0, 255, 0], -1)
-            cv2.circle(frame, [int(left_knee_x), int(left_knee_y)], 3, [0, 255, 0], -1)
+                right_knee_x = results.pose_landmarks.landmark[26].x * frame_w
+                right_knee_y = results.pose_landmarks.landmark[26].y * frame_h
 
-            # Uncomment this for lower frame skeleton
-            # cv2.line(frame, [int(left_hip_x), int(left_hip_y)], [int(right_hip_x), int(right_hip_y)], [0, 0, 255], 1)
-            # cv2.line(frame, [int(left_knee_x), int(left_knee_y)], [int(left_hip_x), int(left_hip_y)], [0, 0, 255], 1)
-            # cv2.line(frame, [int(right_knee_x), int(right_knee_y)], [int(right_hip_x), int(right_hip_y)], [0, 0, 255], 1)
+                left_knee_x = results.pose_landmarks.landmark[25].x * frame_w
+                left_knee_y = results.pose_landmarks.landmark[25].y * frame_h
 
-            x = int((left_hip_x + right_hip_x) / 2)
-            y = int(left_hip_y + 10)
-            cv2.circle(frame, [x, y], 5, [0, 255, 0], -1)
+                # Uncomment this for hip and knee circles
+                # cv2.circle(frame, [int(right_hip_x), int(right_hip_y)], 3, [0, 255, 0], -1)
+                # cv2.circle(frame, [int(left_hip_x), int(left_hip_y)], 3, [0, 255, 0], -1)
+                # cv2.circle(frame, [int(right_knee_x), int(right_knee_y)], 3, [0, 255, 0], -1)
+                # cv2.circle(frame, [int(left_knee_x), int(left_knee_y)], 3, [0, 255, 0], -1)
 
-        # Uncomment this for full skeleton drawing
-        mp_drawing.draw_landmarks(frame, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
-                                  landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+                # Uncomment this for lower frame skeleton
+                # cv2.line(frame, [int(left_hip_x), int(left_hip_y)], [int(right_hip_x), int(right_hip_y)], [0, 0, 255], 1)
+                # cv2.line(frame, [int(left_knee_x), int(left_knee_y)], [int(left_hip_x), int(left_hip_y)], [0, 0, 255], 1)
+                # cv2.line(frame, [int(right_knee_x), int(right_knee_y)], [int(right_hip_x), int(right_hip_y)], [0, 0, 255], 1)
 
-        cv2.imshow("Cock detection", cv2.flip(frame, 1))
-        if cv2.waitKey(10) & 0xFF == ord("q"):
-            break
+                x = int((left_hip_x + right_hip_x) / 2)
+                y = int(left_hip_y + 10)
 
-cam.release()
-cv2.destroyAllWindows()
+                # Uncomment this to put circle on dick
+                # cv2.circle(frame, [x, y], 5, [0, 255, 0], -1)
+
+                send_coords([x, y])
+
+            # Uncomment this for full skeleton drawing
+            # mp_drawing.draw_landmarks(frame, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+            #                           landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+
+            # Uncomment this to open window
+            # cv2.imshow("Cock detection", cv2.flip(frame, 1))
+            # if cv2.waitKey(10) & 0xFF == ord("q"):
+            #     break
+
+except KeyboardInterrupt:
+    cam.release()
+    cv2.destroyAllWindows()
